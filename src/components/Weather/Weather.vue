555785335acc37440cd4a9449d0d8e97fddbc3c9
tremitch504 <trevon@students.alcorn.edu>
2023-07-20 11:16:25 -0500

    Created widgets to be displayed

--- src/components/Weather/Weather.vue
+++ src/components/Weather/Weather.vue
@@ -41,18 +41,18 @@ export default {
     <!-- padding of 3 -->
 <div class="weather">
     <!-- sets margin or padding to .25rem -->
-    <h2 class="day">
+    <h2 class="day">      
       Tuesday
     </h2>
     <!-- margin or padding to 0, color: text-light--> 
-    <p class="date">date</p>
+    <p class="date">date</p>   
     <small>time</small>
     <h2 class="place"> <i class="bi bi-geo-alt">New <small>Orleans</small></i></h2>
     <div class="temp">       
         <h1 class="temp-weather">19&deg;</h1>
-        <!-- color is text-light --> 
+        <!-- color is text-light -->       
         <h2 class="description">description</h2>
-    </div>  
+    </div>     
 </div>
 </div>
 
