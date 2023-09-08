508440f27a1de4d932a2023f5d407c1f8cfb9015
tremitch504 <trevon@students.alcorn.edu>
2023-09-08 00:16:35 -0500

    testing for deployment

--- src/components/HeaderWeather/HeaderWeather.vue
+++ src/components/HeaderWeather/HeaderWeather.vue
@@ -141,7 +141,7 @@ export default {
         <!-- here is to put the search logo -->
         <i class="bi bi-search" v-if="this.city.length === 0"></i> 
         <i class="bi bi-x-circle" v-if="this.city.length !== 0" v-on:click="clearInput"></i>
-          <input class="bar" type="text" v-on:@keyup.enter="searchWeather" v-model="city" v-on:change="filterHandle" placeholder="Search for a city"  autocomplete="off">
+          <input class="bar" type="text" v-on:@keyup.enter="searchWeather" v-model="this.city" v-on:change="filterHandle" placeholder="Search for a city"  autocomplete="off">
         </form>   
     </div>
 
