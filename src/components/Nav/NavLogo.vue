2c2dbf400c479a628ff370c722a6c3a6a82ebcad
tremitch504 <trevon@students.alcorn.edu>
2023-09-20 08:30:58 -0500

    updating Readme again

--- src/components/Nav/NavLogo.vue
+++ src/components/Nav/NavLogo.vue
@@ -9,7 +9,7 @@ import './NavLogo.css'
         <!-- <i class="bi bi-thermometer-sun"></i> -->
         <img class="weather-icon" src="/public/weather-app.png">
         <h1>The Weather App</h1>  
-    </div>
+    </div> 
 
 
 </div>
